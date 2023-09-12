ae53a58aa1ef8860e14d74740a97f8a134841344
LYS-KR <dldustn910@gmail.com>
2023-09-12 19:29:29 +0900

    JS-Home

--- main.js
+++ main.js
@@ -11,3 +11,12 @@ document.addEventListener('scroll', () => {
     }
     
 })
+
+
+// Home 섹션을 아래로 스크롤시 투명하게 처리함
+const home = document.querySelector('.home__container');
+const homeHeight = home.offsetHeight;
+document.addEventListener('scroll', ()=> {
+    console.log(1 - window.scrollY / homeHeight);
+    home.style.opacity = 1 - window.scrollY / homeHeight;
+});
